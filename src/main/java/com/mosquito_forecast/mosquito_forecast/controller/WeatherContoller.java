2baf3c56d016a620da98d44ab3b962acaeb99799
dohyunk58 <hedge3x@gmail.com>
2024-08-26 04:29:36 +0900

    api 키를 이용해 url을 생성하는 기능 추가 WeatherContoller에서 /weather로 접속하면 WeatherService의 updateWeather()가 호출되어 api키를 이용해 get 요청을 보냅니다.
    Http 200 응답은 받았으나 키가 유효하지 않다는 응답을 보냅니다.
    디버깅을 통해 같은 url을 postman이나 브라우저를 통해 접속하면 정상 작동하니 이 부분의 원인을 찾아 수정해야 합니다.

--- src/main/java/com/mosquito_forecast/mosquito_forecast/controller/WeatherContoller.java
+++ src/main/java/com/mosquito_forecast/mosquito_forecast/controller/WeatherContoller.java
@@ -3,15 +3,18 @@ package com.mosquito_forecast.mosquito_forecast.controller;
 import com.mosquito_forecast.mosquito_forecast.dto.weatherDto;
 import com.mosquito_forecast.mosquito_forecast.service.WeatherService;
 import io.swagger.v3.oas.annotations.media.Schema;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping(produces = "application/json;charset=UTF-8")
 public class WeatherContoller {
 
-    private WeatherService weatherService = new WeatherService();
+    @Autowired
+    private WeatherService weatherService;
 
     @GetMapping("/") // 기본 페이지
     public String openWeatherPage()
@@ -20,7 +23,8 @@ public class WeatherContoller {
     }
 
     @GetMapping("/weather")
-    public String updateWeather() {
-        return weatherService.
+    public void updateWeather() {
+        weatherService.updateWeather();
     }
+
 }
--- src/main/java/com/mosquito_forecast/mosquito_forecast/service/WeatherService.java
+++ src/main/java/com/mosquito_forecast/mosquito_forecast/service/WeatherService.java
@@ -1,47 +1,62 @@
 package com.mosquito_forecast.mosquito_forecast.service;
 
 import com.mosquito_forecast.mosquito_forecast.domain.Region;
-import com.mosquito_forecast.mosquito_forecast.domain.Weather;
-import com.mosquito_forecast.mosquito_forecast.dto.weatherDto;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @NoArgsConstructor
 @Service
 public class WeatherService {
 
-    private Weather weather;
+    @Value("${weatherKey}") private String weatherKey;
 
-    @Value("${weatherKey}")
-    private String weatherKey;
-
-    private String baseDate = "20240824";
-
-    private String baseTime = "1200";
-
-    private Region region;
-    //private int nx = region.getNx();
+    private String baseDate = "20240825";
+    private String baseTime = "2300";
     private int nx = 60;
-    //private int ny = region.getNy();
     private int ny = 127;
 
-    private String requestUrl = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst" +
-            "?serviceKey=" + weatherKey + "&numOfRows=10&pageNo=1&dataType=JSON&base_date=" +
-            baseDate + "&base_time=" + baseTime + "&nx=" + nx + "&ny=" + ny;
+    private String requestUrl;
 
-    // private String apiUrl = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst";
+    private final String apiUrl = "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst";
 
-    RestTemplate restTemplate = new RestTemplate();
+    // uriString 값을 저장할 인스턴스 변수
+    private String uriString;
 
-    public weatherDto updateWeather() {
-        ResponseEntity<weatherDto> result = restTemplate.getForEntity(requestUrl, weatherDto.class);
+    public String weatherUrl() {
+        return "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?" +
+                "serviceKey=" + weatherKey + "&numOfRows=10&pageNo=1&dataType=JSON&base_date=" +
+                baseDate + "&base_time=" + baseTime + "&nx=" + nx + "&ny=" + ny;
+    }
 
+    public void weatherUriBuilder() {
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(apiUrl)
+                //.queryParam("serviceKey", requestApiConfig.getWeatherApiEncodingKey())
+                .queryParam("pageNo", 1)
+                .queryParam("numOfRows", 1000)
+                .queryParam("dataType", "JSON")
+                .queryParam("base_date", baseDate)
+                .queryParam("base_time", baseTime)
+                .queryParam("nx", nx)
+                .queryParam("ny", ny);
+
+        this.uriString = uriBuilder.toUriString() + "&serviceKey=" + weatherKey;
+        // 이미 인코딩된 serviceKey 추가 (UriComponentsBuilder 에서 인코딩 가능성이 있음)
+    }
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    public void updateWeather() {
+        // weatherUriBuilder();
+        // ResponseEntity<String> result = restTemplate.getForEntity(this.uriString, String.class);
+        requestUrl = weatherUrl();
+        ResponseEntity<String> result = restTemplate.getForEntity(requestUrl, String.class);
         System.out.println(result.getStatusCode());
         System.out.println(result.getBody());
-
-        return result.getBody();
     }
 }
