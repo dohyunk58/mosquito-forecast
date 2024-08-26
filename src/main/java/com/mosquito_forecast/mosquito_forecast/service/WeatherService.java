bd0b126e47e6b65c8dcb87b314ab60da126f70ad
dohyunk58 <hedge3x@gmail.com>
2024-08-27 03:50:06 +0900

    API를 이용해 JSON 받는 기능 추가 resttemplate에서 string을 인코딩하는 이슈로 이를 URI를 넣어 해결했습니다.

--- src/main/java/com/mosquito_forecast/mosquito_forecast/service/WeatherService.java
+++ src/main/java/com/mosquito_forecast/mosquito_forecast/service/WeatherService.java
@@ -7,56 +7,49 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @NoArgsConstructor
 @Service
 public class WeatherService {
 
     @Value("${weatherKey}") private String weatherKey;
+    @Value("${weatherKeyDecode}") private String weatherKeyDecode;
 
-    private String baseDate = "20240825";
+    private String baseDate = "20240826";
     private String baseTime = "2300";
     private int nx = 60;
     private int ny = 127;
 
-    private String requestUrl;
-
     private final String apiUrl = "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst";
 
-    // uriString 값을 저장할 인스턴스 변수
-    private String uriString;
+    public URI buildWeatherUri() throws URISyntaxException {
+        String requestUrl = apiUrl +
+                "?serviceKey=" + weatherKey +
+                "&pageNo=1" +
+                "&numOfRows=1000" +
+                "&dataType=JSON" +
+                "&base_date=" + baseDate +
+                "&base_time=" + baseTime +
+                "&nx=" + nx +
+                "&ny=" + ny;
 
-    public String weatherUrl() {
-        return "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?" +
-                "serviceKey=" + weatherKey + "&numOfRows=10&pageNo=1&dataType=JSON&base_date=" +
-                baseDate + "&base_time=" + baseTime + "&nx=" + nx + "&ny=" + ny;
-    }
-
-    public void weatherUriBuilder() {
-        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(apiUrl)
-                //.queryParam("serviceKey", requestApiConfig.getWeatherApiEncodingKey())
-                .queryParam("pageNo", 1)
-                .queryParam("numOfRows", 1000)
-                .queryParam("dataType", "JSON")
-                .queryParam("base_date", baseDate)
-                .queryParam("base_time", baseTime)
-                .queryParam("nx", nx)
-                .queryParam("ny", ny);
-
-        this.uriString = uriBuilder.toUriString() + "&serviceKey=" + weatherKey;
-        // 이미 인코딩된 serviceKey 추가 (UriComponentsBuilder 에서 인코딩 가능성이 있음)
+        return new URI(requestUrl);  // Create URI object
     }
 
     @Autowired
     RestTemplate restTemplate;
 
     public void updateWeather() {
-        // weatherUriBuilder();
-        // ResponseEntity<String> result = restTemplate.getForEntity(this.uriString, String.class);
-        requestUrl = weatherUrl();
-        ResponseEntity<String> result = restTemplate.getForEntity(requestUrl, String.class);
-        System.out.println(result.getStatusCode());
-        System.out.println(result.getBody());
+        try{
+            URI uri = buildWeatherUri();
+            ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
+            System.out.println(result.getStatusCode());
+            System.out.println(result.getBody());
+        } catch (URISyntaxException e) {
+            e.printStackTrace();  // Handle possible URI syntax errors
+        }
     }
 }
